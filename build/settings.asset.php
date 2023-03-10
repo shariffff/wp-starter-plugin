c127587bcd468f6fb8b30fe30e8014947aecf016
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2023-03-10 13:15:11 +0600

    fix app id

--- build/settings.asset.php
+++ build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-react-refresh-runtime'), 'version' => '7f9732ade0b2a2e6c491');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '75f9ecc93b1c5925a746');
