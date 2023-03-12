3509888066e7faae1ee62128bdd6ac83ef3940d1
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2023-03-12 21:22:32 +0600

    remove unused functions

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -10,7 +10,12 @@ function admin_menu()
 		__('WP Starter', 'text-domain'),
 		'manage_options',
 		'wp-starter',
-		'admin_page',
+		function () {
+			echo <<<HTML
+				<noscript>Enable JavaScript to run this app.</noscript>
+				<div id="app"></div>
+			HTML;
+		},
 		'dashicons-lightbulb',
 		null
 	);
@@ -19,11 +24,6 @@ function admin_menu()
 }
 
 
-/**
- * Includes asset.
- *
- * @return void
- */
 function prefix_wp_enqueue_scripts()
 {
 	enqueue_scripts_from_asset_file('index', WP_STARTER_PLUGIN_PLUGIN_FILE);
@@ -34,14 +34,6 @@ function admin_page_load()
 	add_action('admin_enqueue_scripts',  'prefix_wp_enqueue_scripts');
 	remove_all_filters('admin_footer_text');
 	remove_filter('update_footer', 'core_update_footer');
-	add_filter('update_footer',  '__return_null');
-	add_filter('admin_footer_text',  '__return_null');
-}
-
-function admin_page()
-{
-	echo <<<HTML
-		<noscript>You need to enable JavaScript to run this app.</noscript>
-		<div id="app"></div>
-	HTML;
+	add_filter('update_footer',  '__return_empty_string');
+	add_filter('admin_footer_text',  '__return_empty_string');
 }
